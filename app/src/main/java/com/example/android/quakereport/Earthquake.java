fb88add66dc76e7ebf89fd087b87841b21e7db6a
flysamc <flysamc@gmail.com>
2017-03-04 01:38:38 +0530

    ui to add data and time in proper format

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,8 +1,11 @@
 package com.example.android.quakereport;
 
+import android.util.Log;
+
 import java.text.SimpleDateFormat;
 
 import static android.text.TextUtils.split;
+import static android.text.TextUtils.substring;
 
 /**
  * Created by flysamc on 1/3/17.
@@ -14,6 +17,7 @@ public class Earthquake {
     private long mTime;
     private String mNearBy;
     private String mLocation;
+    private String[] seperateLocation;
 
 
 
@@ -30,7 +34,11 @@ public class Earthquake {
         return mMagnitude;
     }
     public String getPlace(){
-        return mPlace;
+        return mLocation;
+    }
+    public String getNearBy(){
+
+        return mNearBy;
     }
     public long getTime(){
         return mTime;
@@ -38,7 +46,11 @@ public class Earthquake {
 
     public void splitLocation(String mPlace){
 
-        split(mPlace , ",");
+        seperateLocation = split(mPlace , ",");
+        mLocation = seperateLocation[1];
+        mNearBy = seperateLocation[0];
+        Log.v("Earthquake" , mNearBy);
+        Log.v("Earthquake" , mLocation);
 
     }
 
